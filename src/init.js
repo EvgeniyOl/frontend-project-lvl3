857906a43ab0818018184e68c21263c926c224f2
Евгений Олянский <evgeniy.olyanskii@gmail.com>
2022-03-27 18:18:57 +0600

    validation

---
+++ src/init.js
@@ -0,0 +1,43 @@
+import i18next from "i18next"; //мультиязычная библиотека
+import onChange from "on-change";
+import view from "./view.js";
+import resources from "./locales/index.js";
+import render from "./render.js";
+
+export default () => {
+  const elements = {
+    feedback: document.querySelector(".feedback"),
+    feeds: document.querySelector(".feeds"),
+    form: document.querySelector(".rss-form"),
+    input: document.getElementById("url-input"),
+    posts: document.querySelector(".posts"),
+    submitButton: document.querySelector('button[type="submit"]'),
+  };
+  const state = onChange(
+    {
+      lng: "ru",
+      links: [],
+      input: {
+        readonly: false,
+      },
+      feedback: {
+        error: null,
+        success: null,
+      },
+      valid: true,
+      newFeed: [],
+      newPosts: [],
+      feeds: [],
+      posts: [],
+    },
+    render(elements)
+  );
+
+  const i18nextInstance = i18next.createInstance();
+  i18nextInstance.init({
+    lng: state.lng,
+    resources,
+  });
+
+  view(elements, state, i18nextInstance);
+};
---
+++ src/validation.js
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+const validate = async (link, links, i18n) => {
+  const schema = yup
+    .string()
+    .url(i18n.t("errors.link"))
+    .notOneOf(links, i18n.t("errors.uniq"))
+    .required();
+  try {
+    await schema.validate(link);
+    return null;
+  } catch (e) {
+    return e.message;
+  }
+};
+
+export default validate;
