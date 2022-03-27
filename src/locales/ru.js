cc9ed3994d9fadd5d9607b0bbf634b1d5f8d17f4
Евгений Олянский <evgeniy.olyanskii@gmail.com>
2022-03-27 18:28:52 +0600

    rulang

---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    errors: {
+      link: "Ссылка должна быть валидным URL",
+      uniq: "RSS уже существует",
+      parse: "Ресурс не содержит валидный RSS",
+      netError: "Ошибка сети",
+    },
+    success: "RSS успешно загружен",
+  },
+};
