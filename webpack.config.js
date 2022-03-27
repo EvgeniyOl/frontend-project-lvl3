627c062b722fcf760a7a0b674a7993c316e4261d
Евгений Олянский <evgeniy.olyanskii@gmail.com>
2022-03-27 17:26:40 +0600

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import pkg from "webpack";
+
+const { SourceMapDevToolPlugin } = pkg;
+
+export default {
+  mode: process.env.NODE_ENV || "development",
+  entry: "./src/index.js",
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(path.resolve(), "dist"),
+    },
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new SourceMapDevToolPlugin({
+      filename: "[file].map",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
