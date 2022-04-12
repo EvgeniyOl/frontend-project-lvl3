ca1782bbb7dbf62a00c7c85fc432e592242d09cb
Евгений <evgeniy@Air-Evgenij.Dlink>
2022-04-12 17:25:24 +0600

    parserMove

--- src/view.js
+++ src/view.js
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _ from "lodash"; //??
 import parseXml from "./parser.js";
 import axios from "axios";
 import { setLocale, string } from "yup";
@@ -44,6 +44,31 @@ const downloadXml = (url) => {
       error.type = "networkError";
       throw error;
     });
+};
+
+const parseXml = (content) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(content, "text/xml");
+  const errorElement = doc.querySelector("parsererror");
+  if (errorElement) {
+    const error = new Error("Invalid RSS structure");
+    error.process = "feedLoading";
+    error.type = "invalidFeedXml";
+    throw error;
+  }
+
+  const title = doc.querySelector("channel title").textContent;
+  const description = doc.querySelector("channel description").textContent;
+  const items = [...doc.querySelectorAll("item")].map((item) => ({
+    title: item.querySelector("title").textContent,
+    description: item.querySelector("description").textContent,
+    link: item.querySelector("link").textContent,
+  }));
+  return {
+    title,
+    description,
+    items,
+  };
 };
 
 const saveFeed = (watchedState, feedUrl, feedData) => {
