d31cdec1a1907b9a5a1507a1bbf9210ef82b9efa
Евгений <evgeniy@Air-Evgenij.Dlink>
2022-04-12 17:15:28 +0600

    rebuildRenderFixRenderBugs

--- src/init.js
+++ src/init.js
@@ -1,43 +1,139 @@
 import i18next from "i18next";
 import onChange from "on-change";
-import view from "./view.js";
 import resources from "./locales/index.js";
-import render from "./render.js";
+import {
+  changeLanguage,
+  closeModal,
+  handleReadPost,
+  loadFeed,
+  updateFeeds,
+} from "./view.js";
 
-export default () => {
-  const elements = {
-    feedback: document.querySelector(".feedback"),
-    feeds: document.querySelector(".feeds"),
-    form: document.querySelector(".rss-form"),
-    input: document.getElementById("url-input"),
-    posts: document.querySelector(".posts"),
-    submitButton: document.querySelector('button[type="submit"]'),
+import {
+  renderModal,
+  renderFeedLoadingProcess,
+  renderFeeds,
+  renderFormValidationProcess,
+  renderPosts,
+} from "./render.js";
+
+const app = () => {
+  const defaultLanguage = "ru";
+
+  const uiElements = {
+    body: document.querySelector("body"),
+    form: document.querySelector("form"),
+    formInput: document.querySelector("form #feed-url"),
+    formButton: document.querySelector("form button"),
+    feedback: document.querySelector("#feedback"),
+    spinner: document.querySelector("#spinner"),
+    feedsContainer: document.querySelector("#feeds"),
+    postsContainer: document.querySelector("#posts"),
+    languageSelector: document.querySelector("#language-selector"),
+    modal: document.querySelector("#modal"),
+    modalTitle: document.querySelector("#modal-title"),
+    modalBody: document.querySelector("#modal-body"),
+    modalRead: document.querySelector("#modal-read"),
+    modalClose: document.querySelector("#modal-close"),
   };
-  const state = onChange(
-    {
-      lng: "ru",
-      links: [],
-      input: {
-        readonly: false,
-      },
-      feedback: {
-        error: null,
-        success: null,
-      },
-      valid: true,
-      newFeed: [],
-      newPosts: [],
-      feeds: [],
-      posts: [],
-    },
-    render(elements)
-  );
 
-  const i18nextInstance = i18next.createInstance();
-  i18nextInstance.init({
-    lng: state.lng,
-    resources,
-  });
+  const i18 = i18next.createInstance();
+  i18
+    .init({
+      lng: defaultLanguage,
+      resources,
+    })
+    .then(() => {
+      const state = {
+        language: defaultLanguage,
+        formValidation: {
+          status: "idle",
+          isValid: true,
+          error: null,
+        },
+        feedLoading: {
+          status: "idle",
+          error: null,
+        },
+        feeds: [],
+        posts: [],
+        modal: {
+          isVisible: false,
+          postId: null,
+        },
+        ui: {
+          postReadIds: [],
+        },
+      };
+      const watchedState = onChange(state, (path, value) => {
+        switch (path) {
+          case "language": {
+            i18.changeLanguage(value).then(() => {
+              renderFormValidationProcess(
+                i18,
+                state.formValidation,
+                uiElements
+              );
+            });
+            break;
+          }
+          case "formValidation.status": {
+            renderFormValidationProcess(i18, state.formValidation, uiElements);
+            break;
+          }
+          case "feedLoading.status": {
+            renderFeedLoadingProcess(i18, state.feedLoading, uiElements);
+            break;
+          }
+          case "feeds": {
+            renderFeeds(i18, state, uiElements);
+            break;
+          }
+          case "posts":
+          case "ui.postReadIds": {
+            renderPosts(i18, state, uiElements);
+            break;
+          }
+          case "modal.isVisible": {
+            renderModal(i18, state, uiElements);
+            break;
+          }
+          // no default
+        }
+      });
 
-  view(elements, state, i18nextInstance);
+      uiElements.form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const formData = new FormData(uiElements.form);
+        const feedUrl = formData.get("feed-url").trim();
+        loadFeed(watchedState, feedUrl);
+      });
+
+      uiElements.postsContainer.addEventListener("click", (e) => {
+        const postElement = e.target;
+        handleReadPost(watchedState, postElement);
+      });
+
+      uiElements.languageSelector.addEventListener("click", (e) => {
+        const { language } = e.target.dataset;
+        changeLanguage(watchedState, language);
+      });
+
+      uiElements.modal.addEventListener("click", (e) => {
+        if (e.target.hasAttribute("data-close")) {
+          closeModal(watchedState);
+        }
+      });
+      document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape" && watchedState.modal.isVisible) {
+          closeModal(watchedState);
+        }
+      });
+
+      renderFormValidationProcess(i18, state.formValidation, uiElements);
+
+      updateFeeds(watchedState);
+    });
 };
+
+export default app;
--- src/render.js
+++ src/render.js
@@ -1,186 +1,213 @@
-const clear = (elements) => {
-  elements.input.classList.remove("is-invalid");
-  elements.input.classList.remove("text-success");
-  elements.feedback.classList.remove("text-danger");
-  elements.feedback.classList.remove("text-success");
+const renderIdleStatus = (uiElements) => {
+  uiElements.formInput.removeAttribute("readonly");
+  uiElements.formInput.focus();
+  uiElements.formButton.disabled = false;
+  uiElements.spinner.classList.add("d-none");
 };
 
-const renderError = (elements, value) => {
-  if (value === null) {
-    return;
-  }
-  clear(elements);
-  elements.feedback.textContent = value;
-  elements.input.classList.add("is-invalid");
-  elements.feedback.classList.add("text-danger");
+const renderProcessingStatus = (uiElements) => {
+  uiElements.feedback.textContent = "";
+  uiElements.formButton.disabled = true;
+  uiElements.formInput.setAttribute("readonly", "true");
+  uiElements.spinner.classList.remove("d-none");
 };
 
-const renderSuccess = (elements, value) => {
-  if (value === null) {
-    return;
-  }
-  clear(elements);
-  elements.feedback.textContent = value;
-  elements.feedback.classList.add("text-success");
-  elements.input.value = "";
-  elements.input.focus();
+const renderErrorStatus = (uiElements, errorMessage) => {
+  uiElements.feedback.classList.remove("text-success");
+  uiElements.feedback.classList.add("text-danger");
+  uiElements.feedback.textContent = errorMessage;
+  uiElements.formInput.removeAttribute("readonly");
+  uiElements.formButton.disabled = false;
+  uiElements.spinner.classList.add("d-none");
 };
 
-const createTitle = (field, title) => {
-  //????
-  if (field.querySelector(".card")) {
-    return;
+const renderSuccessStatus = (uiElements, successMessage) => {
+  renderIdleStatus(uiElements);
+  uiElements.feedback.classList.remove("text-danger");
+  uiElements.feedback.classList.add("text-success");
+  uiElements.feedback.textContent = successMessage;
+  uiElements.formInput.value = "";
+};
+
+const renderFormValidationProcess = (i18, formValidationState, uiElements) => {
+  const textElements = document.querySelectorAll("[data-text]");
+  textElements.forEach((element) => {
+    element.textContent = i18.t(element.dataset.text);
+  });
+
+  switch (formValidationState.status) {
+    case "idle": {
+      renderIdleStatus(uiElements);
+      uiElements.formInput.classList.remove("is-invalid");
+      break;
+    }
+    case "validation": {
+      renderProcessingStatus(uiElements);
+      break;
+    }
+    case "error": {
+      uiElements.formInput.classList.add("is-invalid");
+      renderErrorStatus(
+        uiElements,
+        i18.t(`errors.${formValidationState.error}`)
+      );
+      break;
+    }
+    default:
+      throw new Error(
+        `Untracked formValidation status: ${formValidationState.status}`
+      );
   }
-  const div = document.createElement("div");
-  field.appendChild(div);
-  div.classList.add("card", "border-0");
-  const ul = document.createElement("ul");
+};
+
+const renderFeedLoadingProcess = (i18, feedLoadingState, uiElements) => {
+  switch (feedLoadingState.status) {
+    case "idle": {
+      renderIdleStatus(uiElements);
+      break;
+    }
+    case "loading": {
+      renderProcessingStatus(uiElements);
+      break;
+    }
+    case "error": {
+      renderErrorStatus(uiElements, i18.t(`errors.${feedLoadingState.error}`));
+      break;
+    }
+    case "success": {
+      renderSuccessStatus(uiElements, i18.t("success"));
+      break;
+    }
+    default:
+      throw new Error(
+        `Untracked feedLoading status: ${feedLoadingState.status}`
+      );
+  }
+};
+
+const renderFeeds = (i18, state, uiElements) => {
   const card = document.createElement("div");
-  div.appendChild(card);
-  card.classList.add("card-body");
-  div.appendChild(ul);
-  ul.classList.add("list-group", "border-0", "rounded-0");
-  const h2 = document.createElement("h2");
-  card.appendChild(h2);
-  h2.classList.add("card-title", "h4");
-  h2.textContent = title;
-};
+  card.classList.add("card");
 
-const closeModal = (modal, body, div) => {
-  body.classList.remove("modal-open");
-  modal.classList.remove("show");
-  modal.setAttribute("style", "display: none;");
-  body.setAttribute("style", "");
-  div.remove();
-};
+  const h4 = document.createElement("h4");
+  h4.classList.add("card-header");
+  h4.dataset.text = "feedsColumn";
+  h4.textContent = i18.t("feedsColumn");
 
-const changeLink = (link) => {
-  link.classList.remove("fw-bold");
-  link.classList.add("fw-normal", "link-secondary");
-};
-
-const renderModal = (postTitle, postDescription, postLink, link) => {
-  const modal = document.getElementById("modal");
-  const body = document.querySelector("body");
-  body.classList.add("modal-open");
-  body.setAttribute("style", "overflow: hidden; padding-right: 17px;");
-  const modalHeader = document.querySelector(".modal-header");
-  const modalBody = document.querySelector(".modal-body");
-  const modalFooter = document.querySelector(".modal-footer");
-  modal.classList.add("show");
-  modal.setAttribute("style", "display: block;");
-  modal.setAttribute("role", "dialog");
-  modalHeader.querySelector(".modal-title").textContent = postTitle;
-  modalBody.textContent = postDescription;
-  modalFooter.querySelector("a").setAttribute("href", postLink);
-  const div = document.createElement("div");
-  body.appendChild(div);
-  div.classList.add("modal-backdrop", "fade", "show");
-
-  modalHeader.querySelector("button").addEventListener("click", (e) => {
-    e.preventDefault();
-    closeModal(modal, body, div);
-    changeLink(link);
-  });
-
-  modalFooter.querySelector("button").addEventListener("click", (e) => {
-    e.preventDefault();
-    closeModal(modal, body, div);
-    changeLink(link);
-  });
-};
-
-const renderPosts = (elements, posts) => {
-  const fieldPost = elements.posts;
-  const ul = fieldPost.querySelector(".list-group");
-  posts.forEach((post) => {
-    const { postTitle, postDescription, postLink, postId } = post;
+  const feedsList = document.createElement("ul");
+  feedsList.classList.add("list-group", "list-group-flush");
+  state.feeds.forEach((feed) => {
     const li = document.createElement("li");
-    const link = document.createElement("a");
-    const button = document.createElement("button");
-    ul.prepend(li);
+    li.classList.add("list-group-item", "py-2");
+
+    const h6 = document.createElement("h6");
+    h6.classList.add("m-0");
+    h6.textContent = feed.title;
+
+    const p = document.createElement("p");
+    p.classList.add("m-0", "small", "text-secondary");
+    p.textContent = feed.description;
+
+    li.append(h6, p);
+    feedsList.append(li);
+  });
+
+  card.append(h4, feedsList);
+  uiElements.feedsContainer.replaceChildren(card);
+};
+
+const renderPosts = (i18, state, uiElements) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const h4 = document.createElement("h4");
+  h4.classList.add("card-header");
+  h4.dataset.text = "postsColumn";
+  h4.textContent = i18.t("postsColumn");
+
+  const postsList = document.createElement("ul");
+  postsList.classList.add("list-group", "list-group-flush");
+  state.posts.forEach((post) => {
+    const li = document.createElement("li");
     li.classList.add(
       "list-group-item",
       "d-flex",
       "justify-content-between",
       "align-items-start",
-      "border-0",
-      "border-end-0"
+      "py-2"
     );
-    li.prepend(link);
-    link.setAttribute("href", postLink);
-    link.classList.add("fw-bold");
-    link.setAttribute("data-id", postId);
-    link.setAttribute("target", "_blank");
-    link.setAttribute("rel", "noopener noreferrer");
-    link.textContent = postTitle;
-    li.appendChild(button);
-    button.setAttribute("type", "button");
+
+    const a = document.createElement("a");
+    a.classList.add("m-0");
+    if (state.ui.postReadIds.includes(post.id)) {
+      a.classList.add("fw-normal", "link-secondary");
+    } else {
+      a.classList.add("fw-bold");
+    }
+    a.href = post.link;
+    a.target = "_blank";
+    a.textContent = post.title;
+    a.dataset.postId = post.id;
+
+    const button = document.createElement("button");
+    button.type = "button";
     button.classList.add("btn", "btn-outline-primary", "btn-sm");
-    button.setAttribute("data-id", postId);
-    button.setAttribute("data-bs-toggle", "modal");
-    button.setAttribute("data-bs-target", "#modal");
-    button.textContent = "Просмотр";
+    button.textContent = i18.t("viewPostButton");
+    button.dataset.text = "viewPostButton";
+    button.dataset.postId = post.id;
 
-    button.addEventListener("click", (e) => {
-      e.preventDefault();
-      renderModal(postTitle, postDescription, postLink, link);
-      changeLink(link);
-    });
-
-    link.addEventListener("click", () => {
-      changeLink(link);
-    });
+    li.append(a, button);
+    postsList.append(li);
   });
+
+  card.append(h4, postsList);
+  uiElements.postsContainer.replaceChildren(card);
 };
 
-const renderFeeds = (elements, feeds) => {
-  const fieldFeed = elements.feeds;
-  createTitle(fieldFeed, "фиды");
-  createTitle(elements.posts, "посты");
-  const ul = fieldFeed.querySelector(".list-group");
-  feeds.forEach((feed) => {
-    const { feedDescription, feedTitle } = feed;
-    const li = document.createElement("li");
-    const h3 = document.createElement("h3");
+const renderModal = (i18, state, uiElements) => {
+  const { body, modal } = uiElements;
+
+  if (state.modal.isVisible) {
+    const post = state.posts.find(
+      (element) => element.id === state.modal.postId
+    );
+
+    uiElements.modalTitle.textContent = post.title;
     const p = document.createElement("p");
-    ul.prepend(li);
-    li.classList.add("list-group-item", "border-0", "border-end-0");
-    li.appendChild(h3);
-    h3.classList.add("h6", "m-0");
-    h3.textContent = feedTitle;
-    li.appendChild(p);
-    p.classList.add("m-0", "small", "text-black-50");
-    p.textContent = feedDescription;
-  });
-};
+    p.textContent = post.description;
+    uiElements.modalBody.replaceChildren(p);
+    uiElements.modalRead.textContent = i18.t("modal.read");
+    uiElements.modalRead.href = post.link;
+    uiElements.modalClose.textContent = i18.t("modal.close");
 
-const blockInput = (elements, value) => {
-  if (value === true) {
-    elements.input.setAttribute("readonly", value);
-    elements.submitButton.setAttribute("disabled", "");
+    const backdrop = document.createElement("div");
+    backdrop.id = "backdrop";
+    backdrop.classList.add("modal-backdrop", "show");
+    body.append(backdrop);
+    body.classList.add("modal-open");
+
+    modal.classList.add("show");
+    modal.style.display = "block";
+    modal.removeAttribute("aria-hidden");
+    modal.setAttribute("aria-modal", "true");
+    modal.setAttribute("role", "dialog");
   } else {
-    elements.input.removeAttribute("readonly");
-    elements.submitButton.removeAttribute("disabled");
+    modal.classList.remove("show");
+    modal.style.display = "none";
+    modal.removeAttribute("aria-modal");
+    modal.removeAttribute("role");
+    modal.setAttribute("aria-hidden", "true");
+
+    const backdrop = body.querySelector("#backdrop");
+    backdrop.remove();
+    body.classList.remove("modal-open");
   }
 };
 
-const render = (elements) => (path, value) => {
-  if (path === "feedback.error") {
-    renderError(elements, value);
-  }
-  if (path === "feedback.success") {
-    renderSuccess(elements, value);
-  }
-  if (path === "newFeed") {
-    renderFeeds(elements, value);
-  }
-  if (path === "newPosts") {
-    renderPosts(elements, value.reverse());
-  }
-  if (path === "input.readonly") {
-    blockInput(elements, value);
-  }
+export {
+  renderModal,
+  renderFeedLoadingProcess,
+  renderFeeds,
+  renderFormValidationProcess,
+  renderPosts,
 };
-
-export default render;
