923783b13f4296214a370db94760c91a589de4ed
Евгений <evgeniy@Air-Evgenij.Dlink>
2022-04-12 16:36:47 +0600

    parserFixBugsAndRebuild

--- src/parser.js
+++ src/parser.js
@@ -1,19 +1,25 @@
-const getItem = (element) => ({
-  postTitle: element.querySelector("title")?.textContent,
-  postDescription: element.querySelector("description")?.textContent,
-  postLink: element.querySelector("link")?.textContent,
-  postId: element.querySelector("guid")?.textContent,
-});
-
-export default (data, feedback, i18n) => {
-  const domParser = new DOMParser();
-  const xmlDocument = domParser.parseFromString(data, "application/xml");
-  if (xmlDocument.querySelector("parsererror")) {
-    feedback.error = i18n.t("errors.parse");
+const parseXml = (content) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(content, "text/xml");
+  const errorElement = doc.querySelector("parsererror");
+  if (errorElement) {
+    const error = new Error("Invalid RSS structure");
+    error.process = "feedLoading";
+    error.type = "invalidFeedXml";
+    throw error;
   }
+
+  const title = doc.querySelector("channel title").textContent;
+  const description = doc.querySelector("channel description").textContent;
+  const items = [...doc.querySelectorAll("item")].map((item) => ({
+    title: item.querySelector("title").textContent,
+    description: item.querySelector("description").textContent,
+    link: item.querySelector("link").textContent,
+  }));
   return {
-    feedTitle: xmlDocument.querySelector("title")?.textContent,
-    feedDescription: xmlDocument.querySelector("description")?.textContent,
-    feedItems: [...xmlDocument.querySelectorAll("item")].map(getItem),
+    title,
+    description,
+    items,
   };
 };
+export default parseXml;
