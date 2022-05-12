347283e4011260570c524c00e9eb014f39bc74cc
Евгений <evgeniy@Air-Evgenij.Dlink>
2022-05-12 09:11:11 +0600

    add parser

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { setLocale, string } from 'yup';
 import uniqueId from 'lodash/uniqueId.js';
+import parseXml from './parser';
 
 const POST_ID_PREFIX = 'post_';
 const FEED_ID_PREFIX = 'feed_';
@@ -43,32 +44,6 @@ const downloadXml = (url) => {
       error.type = 'networkError';
       throw error;
     });
-};
-
-const parseXml = (content) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(content, 'text/xml');
-  const errorElement = doc.querySelector('parsererror');
-  if (errorElement) {
-    const error = new Error('Invalid RSS structure');
-    error.process = 'feedLoading';
-    error.type = 'invalidFeedXml';
-    throw error;
-  }
-
-  const title = doc.querySelector('channel title').textContent;
-  const description = doc.querySelector('channel description').textContent;
-  const items = [...doc.querySelectorAll('item')]
-    .map((item) => ({
-      title: item.querySelector('title').textContent,
-      description: item.querySelector('description').textContent,
-      link: item.querySelector('link').textContent,
-    }));
-  return {
-    title,
-    description,
-    items,
-  };
 };
 
 const saveFeed = (watchedState, feedUrl, feedData) => {
